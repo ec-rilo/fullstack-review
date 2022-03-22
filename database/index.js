fd21c045fef3285196c6dfbb0b9c2e3f9fcac1b9
Edgar Carrillo <ecarrillo046@gmail.com>
2022-03-22 10:56:18 -0700

    complete save function

--- database/index.js
+++ database/index.js
@@ -1,19 +1,42 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/fetcher', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 let repoSchema = mongoose.Schema({
-  user_id: Number,
-  gh_username: String,
+  id: Number,
   repo_name: String,
-  forks_count: Number
+  forks_count: Number,
+  user_id: Number,
+  user: String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+// only saves repo to db if it is not in there.
+let save = (repo) => {
+  repo = new Repo({
+    id: repo.id,
+    repo_name: repo.name,
+    forks_count: repo.forks_count,
+    user_id: repo.user_id,
+    user: repo.user
+  });
+
+  Promise.resolve(Repo.find({ id: `${repo.id}`}))
+  .then((response) => {
+    if (response.length === 0) {
+      Promise.resolve(repo.save())
+      .catch((err) => {
+        console.error(err);
+      });
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 }
 
 module.exports.save = save;
+module.exports.Repo = Repo;
